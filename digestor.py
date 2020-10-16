e6982a03eb9b225834a5cb0edfddd649bbde1479
heyhyohey <hyoje420@gmail.com>
2020-10-16 12:35:52 +0900

    Add a simple digestion function

--- digestor.py
+++ digestor.py
@@ -1,10 +1,33 @@
 #!/bin/python
 
 import os
+import sys
+import time
 
-# 1. Get remaining space
-pmem_stat = os.popen("df | grep pmem0").read()
-pmem_stat = str(pmem_stat)
-pmem_stat = ' '.join(pmem_stat.split())
-pmem_stat = int(list(pmem_stat.split())[4][0])
-print(pmem_stat)
+# Global variables
+PMEM_HOME="/mnt/pmem/pmem"
+SSD_HOME="/mnt/ssd"
+
+# Migrate files
+def migration():
+    os.system("mv /mnt/pmem/pmem/dummy /mnt/ssd/")
+    print("migration!!!")
+
+# Check remaining space periodically
+while True:
+    pmem_stat = os.popen("df | grep pmem0").read()
+    pmem_stat = str(pmem_stat)
+
+    if pmem_stat == "":
+        print("Warnning: No mounted pmem device")
+        sys.exit(1)
+
+    pmem_stat = ' '.join(pmem_stat.split())
+    pmem_stat = int(list(pmem_stat.split())[4][:-1])
+    print(pmem_stat)
+    
+    # If remaining space is above the threshold
+    if pmem_stat >= 20:
+        migration()
+    time.sleep(1)
+
